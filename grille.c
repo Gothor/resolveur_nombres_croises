ab5dd8019e98bbad7d1f7f2095f8176ce9003397
Rodolphe Peccatte <rodolphe_peccatte@hotmail.com>
2014-11-24 01:59:00 +0100

    Add function to create a grid from a file

--- grille.c
+++ grille.c
@@ -1,10 +1,59 @@
+#include<stdlib.h>
 #include <stdio.h>
 #include "grille.h"
 
-typedef struct grille grille;
-struct grille {
-	int lignes;
-	int colonnes;
-	int* donnees;
+#define TAILLE_MAX 256
+
+/**
+ * Permet de créer une structure Grille depuis un fichier
+ *
+ * @param const char* nomDuFichier
+ *
+ * @return Grille
+ */
+Grille creerGrille(const char* nomDuFichier) {
+	FILE* fichier = NULL;
+	Grille grille;
+	char chaine[TAILLE_MAX] = "";
+	int i, j;
+	
+	fichier = fopen(nomDuFichier, "r");
+
+	if (fichier != NULL) {
+		// Si la lecture du fichier a réussi
+
+		// Lecture du nombre de lignes
+		if (fgets(chaine, TAILLE_MAX, fichier) != NULL)
+			grille.lignes = atoi(chaine);
+
+		// Lecture du nombre de colonnes
+		if (fgets(chaine, TAILLE_MAX, fichier) != NULL)
+			grille.colonnes = atoi(chaine);
+
+		// Récupération des données
+		grille.donnees = malloc(grille.lignes * grille.colonnes * sizeof(int));
+		chaine[1] = '\0';
+		for (i = 0; i < grille.lignes; i++) {
+			for (j = 0; j < grille.colonnes; j++) {
+				if ((chaine[0] = fgetc(fichier)) == 'X')
+					grille.donnees[i * grille.colonnes + j] = CASE_NOIRE;
+				else if (chaine[0] == '-')
+					grille.donnees[i * grille.colonnes + j] = CASE_VIDE;
+				else
+					grille.donnees[i * grille.colonnes + j] = atoi(chaine);
+			}
+			fgetc(fichier); // On passe le saut de ligne
+		}
+
+		fclose(fichier);
+	}
+	else {
+		// Si la lecture du fichier a échoué
+		fprintf(stderr, "Le fichier demandé n'existe pas.");
+
+		exit(EXIT_FAILURE);
+	}
+
+	return grille;
 }
 
--- grille.h
+++ grille.h
@@ -1,5 +1,19 @@
 #ifndef GRILLE_H_INCLUDED
 #define GRILLE_H_INCLUDED
 
+typedef struct Grille Grille;
+struct Grille {
+	int lignes;
+	int colonnes;
+	int* donnees;
+};
+
+enum {
+	CASE_NOIRE = -1,
+	CASE_VIDE = -2
+};
+
+Grille creerGrille(const char* nomDuFichier);
+
 #endif // GRILLE_H_INCLUDED
 
