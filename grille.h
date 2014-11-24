3abf785d62d44e02ed62040ba317008841e13eec
Rodolphe Peccatte <rodolphe_peccatte@hotmail.com>
2014-11-24 23:43:30 +0100

    Add grid's description

--- grille.h
+++ grille.h
@@ -1,6 +1,14 @@
 #ifndef GRILLE_H_INCLUDED
 #define GRILLE_H_INCLUDED
 
+/**
+ * Grille de jeu, cette structure contient la taille de la grille et
+ * son contenu.
+ *
+ * int lignes - nombre de lignes de la grille
+ * int colonnes - nombre de colonnes de la grille
+ * int* donnees - cases de la grille
+ */
 typedef struct Grille Grille;
 struct Grille {
 	int lignes;
